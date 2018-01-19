22b78cec4de9614112c186e7194830cd13b4821c
rurush47 <alex@lichthund.com>
2018-01-19 14:00:50 +0100

    Add command line arguments

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
-from store import Store
+import getopt
+import sys
+
 from utils import genarateBoxTuples
 from tests import *
-import unittest
 from show import showResults
 from copy import deepcopy
-import algorithm
 from beam_search import beam_search
 from tabu_search import beam_search_tabu
 
 
-if __name__ == "__main__":
-    store = Store(width=20, boxes=[Box(a) for a in genarateBoxTuples(25, w_max = 10, h_max = 10)])
+def main(argv):
+    width = 20
+    offset = 1
+    box_quantity = 25
+    max_iterations = 15
+
+    try:
+        opts, args = getopt.getopt(argv, "hw:o:b:i:", ["width=", "offset=", "boxes=", "iterations="])
+        if len(opts) < 1:
+            print('No args provided. Run again with -h option to see how to use. Running program with default values')
+    except getopt.GetoptError:
+        print('test.py -width <magazine width> -offset <offset between boxes> -boxes <number of boxes> -iterations <number of iterations>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('test.py -width <magazine width> -offset <offset between boxes> -boxes <number of boxes> -iterations <number of iterations>')
+            sys.exit()
+        elif opt in ("-w", "--width"):
+            width = int(arg)
+        elif opt in ("-o", "--offset"):
+            offset = int(arg)
+        elif opt in ("-b", "--boxes"):
+            box_quantity = int(arg)
+        elif opt in ("-i", "--iterations"):
+            max_iterations = int(arg)
+
+    boxes = [Box(a) for a in genarateBoxTuples(box_quantity, w_max=10, h_max=10)]
+    store = Store(width, d=offset,  boxes=boxes)
     # store.optimaze = True
     store.d = 2
     store.placeBoxesBottomLeftFit(store.boxes)
@@ -19,8 +45,12 @@ if __name__ == "__main__":
     top_box = store.getTopBox()
     print("Initial H = {}".format(top_box.y + top_box.h))
     store_list = [store,
-                beam_search(store_copy, max_iterations=15, max_children=50),
-                beam_search_tabu(store_copy, max_iterations=15, max_children=50),
-                ]
+                  beam_search(store_copy, max_iterations=max_iterations, max_children=50),
+                  beam_search_tabu(store_copy, max_iterations=max_iterations, max_children=50),
+                  ]
 
     showResults(store_list)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
